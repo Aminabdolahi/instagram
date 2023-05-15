12a039c291c2d769f569c67fd45c33eaaf81360a
Aminabdolahi <112766327+Aminabdolahi@users.noreply.github.com>
2023-05-15 11:12:12 +0430

    install mui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Index } from "./pages/indes";
+import { ThemeProvider } from "styled-components";
+import { them } from "./componants/Theme";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={them}>
+      <Index />
+    </ThemeProvider>
   );
 }
 
